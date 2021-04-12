2b3dc4fa59deb84ab14b231ab905d6502b6c389e
khairul anwar <59588004+khairulanwarDeen@users.noreply.github.com>
2021-04-12 14:01:42 +0800

    all site asset change

---
+++ build/precache-manifest.14aee58446383efb61de78f3c1da56bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02c67e639426319ba2d5500266f77e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfe040e0c65d70cae03b",
+    "url": "/static/css/main.419178c2.chunk.css"
+  },
+  {
+    "revision": "36128ff7f373e5beff7a",
+    "url": "/static/js/2.1442e098.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1442e098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe040e0c65d70cae03b",
+    "url": "/static/js/main.8bb03413.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
